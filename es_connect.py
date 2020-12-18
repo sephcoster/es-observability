6253e735ecd642ed8b88b466c561f1a2db31c73e
Seph Coster <joseph.coster@cfpb.gov>
2020-12-18 16:53:10 -0500

    initial commit

---
+++ es_connect.py
@@ -0,0 +1,35 @@
+import os
+
+from log import setup_logging
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+
+def get_aws_es_connection():
+	es_host = os.environ.get('ES_HOST')
+    awsauth = AWS4Auth(
+		os.environ.get('AWS_ES_ACCESS_KEY'),
+        os.environ.get('AWS_ES_SECRET_KEY'),
+          'us-east-1',
+          'es'
+        )
+    es = Elasticsearch(
+        hosts=[{'host': es_host, 'port': 443}],
+        http_auth=awsauth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection,
+        timeout=1000
+    )
+
+    return es
+
+es = get_aws_es_connection()
+index_name = os.environ.get('ES_INDEX_NAME')
+
+es.
+
+def main():
+	es = get_aws_es_connection()
+
+
+	logger = setup_logging('complaint')
