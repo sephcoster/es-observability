ded9d10ef6fc74e05fb3debdc75c9d92bff237cd
Seph Coster <joseph.coster@cfpb.gov>
2020-12-21 10:43:32 -0500

    indent fixes

--- es_connect.py
+++ es_connect.py
@@ -5,13 +5,15 @@ from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 
 def get_aws_es_connection():
-	es_host = os.environ.get('ES_HOST')
+    es_host = os.environ.get('ES_HOST')
+    
     awsauth = AWS4Auth(
-		os.environ.get('AWS_ES_ACCESS_KEY'),
+        os.environ.get('AWS_ES_ACCESS_KEY'),
         os.environ.get('AWS_ES_SECRET_KEY'),
           'us-east-1',
           'es'
         )
+    
     es = Elasticsearch(
         hosts=[{'host': es_host, 'port': 443}],
         http_auth=awsauth,
@@ -29,16 +31,16 @@ index_name = os.environ.get('ES_INDEX_NAME')
 es.
 
 def main():
-	es = get_aws_es_connection()
-	logger = setup_logging('complaint')
-	logger.info('Cluster Information: ')
-	es.info()
-	logger.info('Cluster Health: ')
-	es.cluster.health()
-	logger.info('Cluster Indices Available: ')
-	es.cat.indices()
-	logger.info('Index value: ')
-	logger.info(index_name)
+    es = get_aws_es_connection()
+    logger = setup_logging('complaint')
+    logger.info('Cluster Information: ')
+    es.info()
+    logger.info('Cluster Health: ')
+    es.cluster.health()
+    logger.info('Cluster Indices Available: ')
+    es.cat.indices()
+    logger.info('Index value: ')
+    logger.info(index_name)
 
 if __name__ == '__main__':
-    main()	
+    main()  
